c1353302b551bd846a7983ac01f9a4294c6f9db0
Kazi Rifat Al Muin <muin2107042@stud.kuet.ac.bd>
2025-03-06 21:56:59 +0600

    Binary Search on x

---
+++ Codeforces/Edu/Binary Search/Step-2/E - Equation.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define eps 1e-8
+
+void solve() {
+    ld c;
+    cin >> c;
+    ld low = eps, high = 2e5, ans = 0;
+    while(1){
+        ld x = low + (high - low) / 2;
+        ld y = x * x + sqrt(x);
+        if(abs(y - c) < eps) {ans = x; break;}
+        else if(y > c) high = x;
+        else low = x;
+    }
+    cout << fixed << setprecision(10) << ans << "\n";
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t = 1, st = clock();
+    // cin >> t;
+    while(t--){
+        solve();
+    }
+    // cerr << "[Time : " << 1000 * (clock() - st) / CLOCKS_PER_SEC << " ms]\n";
+    return 0;
+}
